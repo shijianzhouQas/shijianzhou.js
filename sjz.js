5302ca00d00a613ad8a45279f3d55772fffaccb7
shijianzhouQas <126366234+shijianzhouQas@users.noreply.github.com>
2024-01-01 22:46:18 +0800

    Create sjz.js

---
+++ sjz.js
@@ -0,0 +1,7 @@
+echo "# shijianzhou.js" >> README.md 
+git init 
+git add README.md 
+git commit -m "第一次提交" 
+git分支 -M main 
+git Remote add origin https://github.com/shijianzhouQas/shijianzhou.js。 git
+ git Push -u 起源 main
